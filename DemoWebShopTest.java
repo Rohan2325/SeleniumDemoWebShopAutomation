6e96b6e324670446243b2d58eb498583b7162ccd
Rohan2325 <142516942+Rohan2325@users.noreply.github.com>
2025-01-28 21:10:08 +0530

    Update DemoWebShopTest.java

--- DemoWebShopTest.java
+++ DemoWebShopTest.java
@@ -18,19 +18,20 @@ public class DemoWebShopTest {
         driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://demowebshop.tricentis.com");
+	        driver.manage().window().maximize();
     }
-        @Test
+               @Test
 		public void testSearchandAddToCart() {
-        driver.findElement(By.id("small-searchterms")).sendKeys("laptop");
-        driver.findElement(By.xpath("//input[@class='button-1 search-box-button']")).click();
+                driver.findElement(By.id("small-searchterms")).sendKeys("laptop");
+                driver.findElement(By.xpath("//input[@class='button-1 search-box-button']")).click();
 		driver.findElement(By.xpath("//input[@value='Add to cart']")).click();
 		driver.findElement(By.xpath("//span[text()='Shopping cart']")).click();
 		String productName = driver.findElement(By.xpath("//a[@class='product-name']")).getText();
 		Assert.assertTrue(productName.contains("14.1-inch Laptop"), "Product not added to the cart");
     }
-        @AfterClass
-        public void teardown() {
-        	driver.quit();
+     @AfterClass
+     public void teardown() {
+     driver.quit();
         }
 
 }
