c3381729f5398e20466e320b9174c44b87540379
Rohan2325 <142516942+Rohan2325@users.noreply.github.com>
2025-01-28 21:06:20 +0530

    Add files via upload

---
+++ DemoWebShopTest.java
@@ -0,0 +1,36 @@
+package demoWebShop.Automation;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class DemoWebShopTest {
+	WebDriver driver;
+     
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://demowebshop.tricentis.com");
+    }
+        @Test
+		public void testSearchandAddToCart() {
+        driver.findElement(By.id("small-searchterms")).sendKeys("laptop");
+        driver.findElement(By.xpath("//input[@class='button-1 search-box-button']")).click();
+		driver.findElement(By.xpath("//input[@value='Add to cart']")).click();
+		driver.findElement(By.xpath("//span[text()='Shopping cart']")).click();
+		String productName = driver.findElement(By.xpath("//a[@class='product-name']")).getText();
+		Assert.assertTrue(productName.contains("14.1-inch Laptop"), "Product not added to the cart");
+    }
+        @AfterClass
+        public void teardown() {
+        	driver.quit();
+        }
+
+}
